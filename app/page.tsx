d96f2a5952bcd65f648e5b0bd1e1c7df39ace646
optimusPrime12-code <samisahariar70@gmail.com>
2025-12-13 13:53:54 +0600

    setting up of the projeect is completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import Home from "@/component/Home/Home";
 import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div>sai nav saklaiun</div>
+    <Home></Home>
   );
 }
---
+++ component/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='overflow-hidden'>this is and the memorizing all the machines the converted version!!</div>
+  )
+}
+
+export default Home
